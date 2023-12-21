7b7d32f781b1121c4bbdf611a29245983707d705
aadityapng <aditya.pangestu46@gmail.com>
2023-12-21 21:25:35 +0700

    penambahan chalange

---
+++ test.js
@@ -0,0 +1,9 @@
+let namaDepan = "Aditya"
+let namaBelakang = "Pangestu"
+let gabung = `Nama depan saya adalah ${namaDepan} dan nama depan saya ${namaBelakang}`
+// console.log(gabung);
+
+let data = "Halo nama saya aditya sedang belajar javascript"
+let kataBaru = "aku"
+let dataBaru = data.replace("saya", kataBaru);
+// console.log(dataBaru)
